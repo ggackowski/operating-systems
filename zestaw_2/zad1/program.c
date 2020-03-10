bdc8128f72eaf8727daf5aaebeaf521376f5c76d
Grzegorz Gackowski <greggupl1@gmail.com>
2020-03-10 19:28:43 +0100

    copying and generating in progress

--- zestaw_2/zad1/program.c
+++ zestaw_2/zad1/program.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void copy_lib(FILE * file1, FILE * file2, int buffor_size) {
+char * generate_str(int len) {
+    char * str = calloc(len, sizeof(char));
+    int i;
+    for (i = 0; i < len - 1; ++i)
+        str[i] = rand() % 21 + 65;
+    str[len - 1] = '\0';
+    return str;
+
+}
+
+void lib_generate(char * filename, int num_of_str, int str_len) {
+    
+}
+
+void lib_copy(char * file1, char * file2, int buffor_size) {
+    FILE * f1 = NULL;
+    FILE * f2 = NULL;
+    if ((f1 = fopen(file1, "r")) < 0) 
+        printf("nie udalo sie otworzyc pliku %s\n\n", file1);
+
+    
+    if ((f2 = fopen(file2, "w")) < 0) 
+        printf("nie udalo sie otworzyc pliku %s\n\n", file2);
+
     char * buffor = calloc(buffor_size, sizeof(char));
     int cnt = 0;
     while ((cnt = fread(buffor, sizeof(char), buffor_size, file1))) {
-
         fwrite(buffor, sizeof(char), cnt, file2);
-
     }
+    fclose(f1);
+    fclose(f2);
 }
 
 int main() {
