f160a2fe6c0adb5339421f6cae31c9ac3a550b11
Grzegorz Gackowski <greggupl1@gmail.com>
2020-03-27 19:35:25 +0100

    what if zad 4a is finished?????

--- zestaw_4/zad4a/catcher.c
+++ zestaw_4/zad4a/catcher.c
@@ -8,16 +8,70 @@
 #include <signal.h>
 #include <string.h>
 
+int usr1_cnt = 0;
+int sending = 0;
+int sigq = 0;
+int rt = 0;
+pid_t sender_pid;
+
 void handler_sigusr1(int sig, siginfo_t * info, void * ucontext) {
-    printf("SA_SIGINFO flag is turned on\n");
-    printf("%d\n", info->si_uid);
+    if (info->si_value.sival_int == 1) {
+        sigq  = 1;
+    }
+    usr1_cnt++;
+    sender_pid = info->si_pid;
+}
+
+void handler_sigusr2(int sig) {
+    sending = 1;
+}
+
+void handler_sigrt1(int sig, siginfo_t * info, void * ucontext) {
+    rt = 1;
+    usr1_cnt++;
+    sender_pid = info->si_pid;
+
 }
 
 int main(int argc, char ** argv) {
     printf("pid: %d\n", getpid());
-    
-    struct sigaction act;
-    act.sa_flags = SA_SIGINFO;
-    act.sa_sigaction = handler_sigusr1;
-    sigaction(SIGUSR1, &act, NULL);
+    signal(SIGUSR2, handler_sigusr2);
+    signal(SIGRTMIN + 2, handler_sigusr2);
+
+    struct sigaction act1;
+
+    act1.sa_flags = SA_SIGINFO;
+    act1.sa_sigaction = handler_sigusr1;
+    sigaction(SIGUSR1, &act1, NULL);
+    struct sigaction act2;
+
+    act2.sa_flags = SA_SIGINFO;
+    act2.sa_sigaction = handler_sigrt1;
+    sigaction(SIGRTMIN + 1, &act2, NULL);
+
+
+    while (1) {
+        if (sending) {
+            if (rt == 1) {
+                for (int i = 0; i < usr1_cnt; ++i) 
+                    kill(sender_pid, SIGRTMIN + 1);
+                kill(sender_pid, SIGRTMIN + 2);
+                printf("[Catcher] I got and sent %d SIGUSR1\n", usr1_cnt);
+                return 0;
+            }
+            else {
+                for (int i = 0; i < usr1_cnt; ++i) {
+                 if (sigq == 1) {
+                    sigqueue(sender_pid, SIGUSR1, (union sigval) (i + 1));
+                }
+                else {
+                    kill(sender_pid, SIGUSR1);
+                }
+            }
+            kill(sender_pid, SIGUSR2);
+            printf("[Catcher] I got and sent %d SIGUSR1\n", usr1_cnt);
+            return 0;
+            }
+        }
+    }
 }
