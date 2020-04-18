9006b617f835ded85296a954fd1569fdf72e65da
Grzegorz Gackowski <greggupl1@gmail.com>
2020-04-18 10:38:24 +0200

    sth

--- zestaw_6/zad1/server.c
+++ zestaw_6/zad1/server.c
@@ -9,23 +9,48 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
+#include <errno.h>
+typedef struct {
+    long mtype;
+    char mtext[100];
+} msgbuf;
+
+int show_err(char * where) {
+    if (errno) {
+        printf("%s: %s\n", where, strerror(errno));
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char ** argv) {
-    key_t key = ftok("$HOME", 'c');
-    int queue = msgget(key, IPC_CREAT);
+    key_t key = ftok(getenv("HOME"), 1);
     
-    struct msgbuf message;
-    message.mtype = 0;
-    message.mtext = calloc(100, sizeof(char));
+    show_err("Key create error");
+    
+    int queue = msgget(key, IPC_CREAT | 0777);
+    
+    show_err("Queue make error");
+    
+
+    msgbuf message;
+    message.mtype = 1;
     strcpy(message.mtext, "test komunikatu");
+    
+    msgsnd(queue, &message, sizeof(message.mtext), IPC_NOWAIT);
 
-    msgsnd(queue, message, 100 * sizeof(char), 0);
+    show_err("message send error");
 
-    struct msgbuf response;
-    response.mtype = 0;
-    response.mtext = calloc(100, sizeof(char));    
-    msgrcv(queue, response, 100 * sizeof(char), 0, 0);
 
+    msgbuf response;
+    response.mtype = 2;
+    
+    msgrcv(queue, &response, sizeof(response.mtext), 0, MSG_NOERROR | IPC_NOWAIT);
+    
+    show_err("message receive error");
+        
     printf("%s\n", response.mtext);
+    
+    msgctl(queue, IPC_RMID, NULL);
 
 }
